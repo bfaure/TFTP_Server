c2fc966c85b0b85b8484510a76a05faf424d897f
bfaure <bfaure23@gmail.com>
2017-04-12 16:34:06 -0400

    added intial files

---
+++ server.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <math.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+
+int main(int argc, char ** argv)
+{
+	printf("Hello World\n");
+
+	return 1;
+}
